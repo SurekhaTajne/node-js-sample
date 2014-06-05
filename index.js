d35884265e2bc7a446f06774419527d6d0fec9e6
sumasoft <surekha.tajne@sumasoft.net>
2014-06-05 12:42:21 +0530

    Added method test

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
+app.get('/test', function(request, response) {
+    response.send('Hello World!')
+})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
